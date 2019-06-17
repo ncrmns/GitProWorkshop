b1bad29846f443d613446e66d40e7d2c8e8f3e32
ncrmns <polhossy@gmail.com>
2019-06-17 16:20:47 +0200

    a3

--- app.js
+++ app.js
@@ -3,14 +3,9 @@ function greet(param){
   console.log(param);
 }
 
-<<<<<<< HEAD
 
 function cheer(word){
   console.log(word);
-=======
-function cheer(){
-  console.log('Malachite!');
->>>>>>> 8a67130a4ea65deea0d3a9ef501b82e6cc807c41
 }
 function count() {
 	console.log('6');
