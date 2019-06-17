ef50c64b8bec2b0461c291a88ecb3db5f1145c59
SamuZad <zadori.samu@gmail.com>
2019-06-17 16:49:51 +0200

    c6

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ function greet(param){
   console.log('Greetings!');
 }
 
-
 function cheer(param1, param2){
   console.log(param1);
   console.log(param2);
 }
-function count() {
-	console.log('6');
+
+function count(word){
+	console.log(`Counting the countless ${word}`);
 }
