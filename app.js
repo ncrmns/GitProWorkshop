1a1ed1512a6957c1925b8b3e43a618534f58be1d
ncrmns <polhossy@gmail.com>
2019-06-17 16:08:56 +0200

    conflict solved

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ function greet(){
   console.log('ncrmns');
 }
 
-<<<<<<< HEAD
 
 function cheer(){
   console.log('Malachite!');
 }
-=======
 function count() {
 	console.log('28');
 }
->>>>>>> 7e350dda66b6382598ad2dc77169e8d1ae677cbd
